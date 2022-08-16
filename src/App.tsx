921f67ea2b8bcf3457249a47a9f858832d792a36
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-08-16 10:15:37 +0300

    fon

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import {AnalogClock} from "./Clock/AnalogClock";
-import {DigitalClock} from "./Clock/DigitalClock";
-import styles from "./Clock/Clock.module.scss"
+import { AnalogClock } from './Clock/AnalogClock';
+import { DigitalClock } from './Clock/DigitalClock';
+import styles from './Clock/Clock.module.scss';
 
 function App() {
+  const [toggleClock, setToggleClock] = useState<boolean>(true);
 
-    const [toggleClock, setToggleClock] = useState<boolean>(true)
-
-    return (
-        <div className="App">
-            {toggleClock ? <AnalogClock/> : <DigitalClock/>}
-            <div className={styles.buttonStyle} onClick={() => setToggleClock(!toggleClock)}>
-                <div>change clock</div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <AnalogClock />
+    </div>
+  );
 }
 
 export default App;
